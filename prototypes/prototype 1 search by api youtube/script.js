8c4cfb16ccd0e8bc5810dd498ef184490f373534
soukayna00 <soukayna.zaidi06@gmail.com>
2022-06-03 15:40:40 +0100

    update

--- prototypes/prototype 1 search by api youtube/script.js
+++ prototypes/prototype 1 search by api youtube/script.js
@@ -1,30 +1,33 @@
 $(document).ready(function(){
 
-var API_KEY="AIzaSyDx1ysoAa0fRENT09k59a4WQK0fW6dFCY0"
-var video='';
-
-
-
-
-$("#form").submit(function(event){
-
-  event.preventDefault()
-  alert("form is prevented")
-
-  var search$("#search").val();
-  VideoSearch(API_KEY,search,10)
-
-
-})
-function VideoSearch(key,search,maxResults){
-
-  $.get("https://www.googleapis.com/youtube/v3/search?key="+key
-  +"&type=video&part=snippet&maxResults="+maxResults+"&q="+search,function(data){})
-  console.lof(data)
-  data.items.forEach(items => {
-    video=`
-    <iframe width="420" height="315" src="https://www.youtube.com/embed/${items.id.videoId}" frameborder="0" allowfullscreen></iframe>`  
-  $("#videos").append(video)
+  var API_KEY="AIzaSyDdi8y1wCTzDGzluQ3rEEYlg5qrFZ00ttU"
+  var video='';
+  
+  
+  
+  
+  $("#form").submit(function(event){
+  
+    event.preventDefault()
+    
+  
+    var search = $("#search").val();
+    VideoSearch(API_KEY,search,20)
+  
+  
+  })
+  function VideoSearch(key,search,maxResults){
+  
+    $.get("https://www.googleapis.com/youtube/v3/search?key="+key
+    +"&type=video&part=snippet&maxResults="+maxResults+"&q="+search,function(data){
+      console.log(data)
+      data.items.forEach(items => {
+        video=`
+        <iframe width="420" height="315" src="https://www.youtube.com/embed/${items.id.videoId}" frameborder="0" allowfullscreen></iframe>
+        <button>add to Favoris</button>`  
+      $("#videos").append(video)
+      });
+    })
+ 
+  }
   });
-}
-});
